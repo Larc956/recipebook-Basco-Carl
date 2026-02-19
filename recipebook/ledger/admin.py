b91caa5858f26cea5dc3f4fc17493a8a58f90111
Larc956 <carl.basco@student.ateneo.edu>
2026-02-19 20:06:21 +0800

    Configured admin.py.

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Recipe, Ingredient, RecipeIngredient
+
+class RecipeIngredientInLine(admin.TabularInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeIngredientInLine,]
+    search_fields = ('name',)
+    list_display = ('name',)
+
+    fieldsets = [
+        ('Details', {
+            'fields': [
+                'name'
+            ]
+        })
+    ]
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+    search_fields = ('quantity',)
+    list_display = ('quantity', 'ingredient', 'recipe')
+    list_filter = ('recipe', 'ingredient')
+
+    fieldsets = [
+        ('Details', {
+            'fields': [
+                ('quantity', 'ingredient'), 'recipe' 
+            ]
+        })
+    ]
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
