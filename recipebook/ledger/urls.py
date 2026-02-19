38319b35499392740ac3bb099e59c1533e89cdc9
Larc956 <carl.basco@student.ateneo.edu>
2026-02-19 20:53:32 +0800

    Clean up and Debugged, Removed hardcoded recipes.

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -4,7 +4,6 @@ from .views import index, recipe_list, recipe_detail
 app_name = "ledger"
 
 urlpatterns = [
-    path('', index, name='index'),
     path('recipes/list', recipe_list, name='recipe_list'),
     path('recipe/<int:recipe_id>', recipe_detail, name='recipe_detail'),
 ]
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,139 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Recipe
 
 def index(request):
     return HttpResponse('Recipe Book')
         
 def recipe_list(request):
     ctx = {
-    "recipes": [
-        {
-            "name": "Recipe 1",
-            "ingredients": [
-                {
-                    "name": "tomato",
-                    "quantity": "3pcs"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1kg"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1L"
-                },
-                {
-                    "name": "sinigang mix",
-                    "quantity": "1 packet"
-                }
-            ],
-            "link": "/recipe/1" 
-        },
-        {
-            "name": "Recipe 2",
-            "ingredients": [
-                {
-                    "name": "garlic",
-                    "quantity": "1 head"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "vinegar",
-                    "quantity": "1/2cup"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1 cup"
-                },
-                {
-                    "name": "salt",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "whole black peppers",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1 kilo"
-                }
-            ],
-            "link": "/recipe/2"
-        }
-    ]
+        "recipes": Recipe.objects.all()
     }
     return render(request, "ledger/recipe_list.html", ctx)
 
 def recipe_detail(request, recipe_id):
-    if recipe_id == 1:
-        ctx = {
-            "name": "Recipe 1",
-            "ingredients": [
-                {
-                    "name": "tomato",
-                    "quantity": "3pcs"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1kg"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1L"
-                },
-                {
-                    "name": "sinigang mix",
-                    "quantity": "1 packet"
-                }
-            ],
-            "link": "/recipe/1"
-        }
-    elif recipe_id == 2:
-        ctx = {
-            "name": "Recipe 2",
-            "ingredients": [
-                {
-                    "name": "garlic",
-                    "quantity": "1 head"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "vinegar",
-                    "quantity": "1/2cup"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1 cup"
-                },
-                {
-                    "name": "salt",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "whole black peppers",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1 kilo"
-                }
-            ],
-            "link": "/recipe/2"
-        }
-    else:
-        ctx = {}
+    ctx = {
+        "recipe": Recipe.objects.get(id=recipe_id)
+    }
     return render(request, "ledger/recipe_detail.html", ctx)
