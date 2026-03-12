3a65b8766895a353f9c34bbd28d62e0f5e1f8480
Larc956 <carl.basco@student.ateneo.edu>
2026-03-12 22:33:55 +0800

    Added RecipeImage to admin and updated recipe_detail template to display images.

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient
-
+from .models import Recipe, RecipeIngredient, RecipeImage
 
 class RecipeIngredientInLine(admin.TabularInline):
     model = RecipeIngredient
 
+class RecipeImageInLine(admin.TabularInline):
+    model = RecipeImage
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     inlines = [
         RecipeIngredientInLine,
+        RecipeImageInLine,
     ]
     search_fields = ("name",)
     list_display = ("name",)
 
     fieldsets = [("Details", {"fields": ["name"]})]
-
 
 admin.site.register(Recipe, RecipeAdmin)
